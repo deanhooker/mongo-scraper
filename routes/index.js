11775b64b3013903c8606ab973f2c13251cf85fb
Dean Hooker <deanhooker91@gmail.com>
2018-03-21 20:17:17 -0600

    views

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var db = require("../models");
+
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        
+    });
+
+    app.get("/saved", function (req, res) {
+        
+    });
+}
--- server.js
+++ server.js
@@ -18,8 +18,22 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/saved', function (req, res) {
+    res.render('saved');
+});
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI, {
+//   useMongoClient: true
+// });
 
 // Start the server
 app.listen(PORT, function () {
